080a22cf4038939de11a9f0281ec8947bf73dcd5
june lee <melodysdreamj@gmail.com>
2024-05-24 08:48:24 +0900

    change index

--- lib/util/_/build_app/function/before_run_app/widget_binding_lego/_.dart
+++ lib/util/_/build_app/function/before_run_app/widget_binding_lego/_.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../../../../main.dart';
 
-@ReadyBeforeRunApp(index: 0.9)
+@ReadyBeforeRunApp(index: 1.0)
 Future<void> readyForWidgetBindingLego() async {
   WidgetsFlutterBinding.ensureInitialized();
 }
