4fe0d34a62f573c9fbaffc65b5b26e29bce4110a
june lee <melodysdreamj@gmail.com>
2024-07-31 17:41:38 +0900

    `

--- lib/util/_/build_app/function/before_run_app/beamer_lego/_.dart
+++ lib/util/_/build_app/function/before_run_app/beamer_lego/_.dart
@@ -1,31 +1,31 @@
-import 'package:beamer/beamer.dart';
-import 'package:flutter/material.dart';
-
-import '../../../../../../main.dart';
-import '../../../../../config/beamer_lego/_.dart';
-import '../../../../shared_params/_/material_app.dart';
-import '../../../widget/material_app/_.dart';
-
-@ReadyBeforeRunApp(index: 1.0)
-Future<void> readyForBeamerLego() async {
-  setBeamerPathUrlStrategy();
-
-  currentMaterialApp = MaterialApp.router(
-    routeInformationParser: BeamerParser(),
-    routerDelegate: beamer_router,
-    backButtonDispatcher: MaterialAppParams.backButtonDispatcher,
-    localizationsDelegates: MaterialAppParams.localizationsDelegates,
-    supportedLocales: MaterialAppParams.supportedLocales ?? const <Locale>[Locale('en', 'US')],
-    locale: MaterialAppParams.locale,
-    title: MaterialAppParams.appName ?? 'June',
-    theme: MaterialAppParams.lightTheme,
-    darkTheme: MaterialAppParams.darkTheme,
-    themeMode: MaterialAppParams.themeMode ?? ThemeMode.system,
-    debugShowCheckedModeBanner: false,
-    builder: (context, child) {
-      return materialAppInsideBuilder(context, child);
-    },
-  );
-
-
-}
+// import 'package:beamer/beamer.dart';
+// import 'package:flutter/material.dart';
+//
+// import '../../../../../../main.dart';
+// import '../../../../../config/beamer_lego/_.dart';
+// import '../../../../shared_params/_/material_app.dart';
+// import '../../../widget/material_app/_.dart';
+//
+// @ReadyBeforeRunApp(index: 1.0)
+// Future<void> readyForBeamerLego() async {
+//   setBeamerPathUrlStrategy();
+//
+//   currentMaterialApp = MaterialApp.router(
+//     routeInformationParser: BeamerParser(),
+//     routerDelegate: beamer_router,
+//     backButtonDispatcher: MaterialAppParams.backButtonDispatcher,
+//     localizationsDelegates: MaterialAppParams.localizationsDelegates,
+//     supportedLocales: MaterialAppParams.supportedLocales ?? const <Locale>[Locale('en', 'US')],
+//     locale: MaterialAppParams.locale,
+//     title: MaterialAppParams.appName ?? 'June',
+//     theme: MaterialAppParams.lightTheme,
+//     darkTheme: MaterialAppParams.darkTheme,
+//     themeMode: MaterialAppParams.themeMode ?? ThemeMode.system,
+//     debugShowCheckedModeBanner: false,
+//     builder: (context, child) {
+//       return materialAppInsideBuilder(context, child);
+//     },
+//   );
+//
+//
+// }
